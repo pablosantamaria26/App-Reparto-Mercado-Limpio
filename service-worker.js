a87dea73386259084fc84b98a981b8385550150c
pablosantamaria26 <santamariapablodaniel@gmail.com>
2025-07-21 12:26:19 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,21 @@
 const CACHE_NAME = "reparto-cache-v1";
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/manifest.json"
+  "./",
+  "./index.html",
+  "./manifest.json"
 ];
 
-// Instalar service worker y guardar archivos en caché
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-// Activar y limpiar caches viejos
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-      );
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-// Interceptar solicitudes y usar caché si está offline
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
+
