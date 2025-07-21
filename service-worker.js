c11f0296308c7d1571787d5891c654557cb90615
pablosantamaria26 <santamariapablodaniel@gmail.com>
2025-07-21 12:35:39 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,18 @@
-const CACHE_NAME = "reparto-cache-v1";
+const CACHE_NAME = 'reparto-v1';
 const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json"
+  'index.html',
+  'manifest.json'
 ];
 
-self.addEventListener("install", event => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("fetch", event => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
-
